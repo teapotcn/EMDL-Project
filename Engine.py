84b3780e417026a55b6a481ad99237dd8b84933f
Zaxiu <zzx0402zzx@foxmail.com>
2021-12-04 15:49:50 +0800

    1

--- Engine.py
+++ Engine.py
@@ -1,12 +1,11 @@
 import requests, json
 from tqdm import tqdm
 from you_get import common
-
+headers = {'User-Agent': "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET4.0C; .NET4.0E; rv:11.0) like Gecko"}
 
 def wyydownloader(ID):
     '''利用官方API实现。'''
     print('正在加载网易云音乐下载器：')
-    headers = {'User-Agent': "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET4.0C; .NET4.0E; rv:11.0) like Gecko"}
     data = requests.get('http://music.163.com/api/song/detail/?id='+ID+'&ids=%5B'+ID+'%5D', headers=headers)
     n = json.loads(data.content)
     name = n['songs'][0]['name']
@@ -25,3 +24,21 @@ def wyydownloader(ID):
 def bilibiliDownloader(url):
     '''用you-get实现。'''
     common.any_download(url, info_only=False,output_dir=r'保存的目录',merge=True)
+def searcher(name):
+    f, c, d, k, _ = [], [], [], [], []
+    data = requests.get(r'http://music.163.com/api/search/get/web?csrf_token=hlpretag=&hlposttag=&s={'+name+r'}&type=1&offset=0&total=true&limit=20',headers=headers)
+    n = json.loads(data.content)['result']['songs'][0:9]
+    for x in range(len(n)):
+        f.append(n[x]['name'])
+        c.append(n[x]['artists'][0]['name'])
+        d.append(n[x]['id'])
+    for y in range(len(n)):
+        k.append([f[y],c[y],str(d[y])])
+    for z in range(len(n)):
+        _.append(('['+str(z)+']')+" | ".join(k[z]))
+    for __ in range(len(n)):
+        print(_[__])
+    a = input('请输入你选择下载的歌曲: ')
+    wyydownloader(str(d[int(a)]))
+def playlist(ID):
+    pass
